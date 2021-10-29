1b98d27d9cc8b717b2ff50438793586fa4b5914d
long vu duc <vuduclong10a1cma@gmail.com>
2021-10-29 18:24:19 +0700

    add

--- buoi1_chuong4.m
+++ buoi1_chuong4.m
@@ -14,21 +14,27 @@ fc = 300;
 fmax = 2*15;
 fnyquist = 2*fmax;
 fml = 16*fnyquist;
+A = 2
 
-t = [0:1/fmax:1];
+% t = [0:1/fmax:1];
+% 
+% s=2.*cos(20*pi*t+pi/4)+cos(30*pi*t);
 
-s=2.*cos(20*pi*t+pi/4)+cos(30*pi*t);
-
-figure
-subplot(2,2,1);
-plot(t,s)
-title('lay mau nyquist');
+% figure
+% subplot(2,2,1);
+% plot(t,s)
+% title('lay mau nyquist');
 
 t2 = [0:1/fml:1];
-s2=2.*cos(20*pi*t2+pi/4)+cos(30*pi*t2);
-subplot(2,2,2);
+s2 = 2.*cos(20*pi*t2+pi/4)+cos(30*pi*t2);
+subplot(2,1,1);
 plot(t2,s2)
-title('lay mau theo matlab');
+title('Tin hieu lay mau theo matlab');
+
+y = ammod(s2,fc,fml,A);
+subplot(2,1,2)
+plot(t2,y)
+title('Tin hieu dieu che AM');
 
 
 
